ec88a9f0ddd9791537688272efa354eced7bf151
Alex-A-B <alexjamesbelton@gmail.com>
2021-07-22 18:50:47 +0100

    commit tree f023d9e9d4e3041dfc92fd78eeb060247a5b4e19

--- src/components/HogFilter.js
+++ src/components/HogFilter.js
@@ -4,7 +4,7 @@ function HogFilter( { handleFilterChange } ) {
         <div className="filterWrapper">
             <label>How would you like your Hogs? </label>
             <select name="filter" onChange={handleFilterChange}>
-                <option value="All">All</option> 
+                <option value="All">All of 'em</option> 
                 <option value="true">Greased</option>
                 <option value="false">Ungreased</option>
            </select>
